34eb46532d17554660c1879bf7fb2dda9b7d6a5e
Mohamedmoataz-s <mohamed.sallam@ejust.edu.eg>
2025-11-03 21:01:09 +0200

    Add files via upload

---
+++ mozmoz_eco.tcl
@@ -0,0 +1,143 @@
+###############################################################################
+# Change list, formatted for IC Compiler
+###############################################################################
+current_instance
+insert_buffer [get_pins {U111/A}] BUF_X2 -new_net_names {net1} -new_cell_names {U367}
+insert_buffer [get_pins {pc_current_reg_13_/D}] BUF_X1 -new_net_names {net2} -new_cell_names {U368}
+insert_buffer [get_pins {pc_current_reg_12_/D}] BUF_X1 -new_net_names {net3} -new_cell_names {U369}
+insert_buffer [get_pins {U114/A}] BUF_X2 -new_net_names {net4} -new_cell_names {U370}
+insert_buffer [get_pins {U105/A}] BUF_X2 -new_net_names {net5} -new_cell_names {U371}
+insert_buffer [get_pins {U119/A}] BUF_X1 -new_net_names {net6} -new_cell_names {U372}
+insert_buffer [get_pins {U131/A}] BUF_X2 -new_net_names {net7} -new_cell_names {U373}
+insert_buffer [get_pins {U192/A2}] BUF_X2 -new_net_names {net8} -new_cell_names {U374}
+insert_buffer [get_pins {U162/A}] BUF_X1 -new_net_names {net9} -new_cell_names {U375}
+insert_buffer [get_pins {U140/A}] BUF_X2 -new_net_names {net10} -new_cell_names {U376}
+insert_buffer [get_pins {U148/A}] BUF_X2 -new_net_names {net11} -new_cell_names {U377}
+insert_buffer [get_pins {U320/A}] BUF_X2 -new_net_names {net12} -new_cell_names {U378}
+insert_buffer [get_pins {U168/A}] BUF_X2 -new_net_names {net13} -new_cell_names {U379}
+insert_buffer [get_pins {U328/A}] BUF_X2 -new_net_names {net14} -new_cell_names {U380}
+insert_buffer [get_pins {U288/Z}] BUF_X1 -new_net_names {net15} -new_cell_names {U381}
+insert_buffer [get_pins {U83/A}] BUF_X2 -new_net_names {net16} -new_cell_names {U382}
+insert_buffer [get_pins {U72/A}] BUF_X2 -new_net_names {net17} -new_cell_names {U383}
+insert_buffer [get_pins {U60/A}] BUF_X1 -new_net_names {net18} -new_cell_names {U384}
+insert_buffer [get_pins {pc_current_reg_14_/D}] BUF_X1 -new_net_names {net19} -new_cell_names {U385}
+insert_buffer [get_pins {pc_current_reg_13_/D}] BUF_X2 -new_net_names {net20} -new_cell_names {U386}
+insert_buffer [get_pins {pc_current_reg_12_/D}] BUF_X2 -new_net_names {net21} -new_cell_names {U387}
+insert_buffer [get_pins {U367/A}] BUF_X1 -new_net_names {net22} -new_cell_names {U388}
+insert_buffer [get_pins {U119/A}] BUF_X2 -new_net_names {net23} -new_cell_names {U389}
+insert_buffer [get_pins {U370/A}] BUF_X1 -new_net_names {net24} -new_cell_names {U390}
+insert_buffer [get_pins {pc_current_reg_11_/D}] BUF_X1 -new_net_names {net25} -new_cell_names {U391}
+insert_buffer [get_pins {U105/A}] BUF_X2 -new_net_names {net26} -new_cell_names {U392}
+insert_buffer [get_pins {pc_current_reg_10_/D}] BUF_X1 -new_net_names {net27} -new_cell_names {U393}
+insert_buffer [get_pins {pc_current_reg_9_/D}] BUF_X1 -new_net_names {net28} -new_cell_names {U394}
+insert_buffer [get_pins {U131/A}] BUF_X2 -new_net_names {net29} -new_cell_names {U395}
+insert_buffer [get_pins {U375/A}] BUF_X1 -new_net_names {net30} -new_cell_names {U396}
+insert_buffer [get_pins {U186/A2}] BUF_X2 -new_net_names {net31} -new_cell_names {U397}
+insert_buffer [get_pins {U376/A}] BUF_X1 -new_net_names {net32} -new_cell_names {U398}
+insert_buffer [get_pins {U183/A2}] BUF_X2 -new_net_names {net33} -new_cell_names {U399}
+insert_buffer [get_pins {U377/A}] BUF_X1 -new_net_names {net34} -new_cell_names {U400}
+insert_buffer [get_pins {U204/A2}] BUF_X2 -new_net_names {net35} -new_cell_names {U401}
+size_cell {U374} {CLKBUF_X1}
+insert_buffer [get_pins {U382/A}] BUF_X1 -new_net_names {net36} -new_cell_names {U402}
+insert_buffer [get_pins {U383/A}] BUF_X1 -new_net_names {net37} -new_cell_names {U403}
+insert_buffer [get_pins {U384/A}] BUF_X1 -new_net_names {net38} -new_cell_names {U404}
+insert_buffer [get_pins {U388/A}] BUF_X2 -new_net_names {net39} -new_cell_names {U405}
+insert_buffer [get_pins {U113/Z}] BUF_X2 -new_net_names {net40} -new_cell_names {U406}
+size_cell {U371} {CLKBUF_X1}
+insert_buffer [get_pins {U105/A}] BUF_X2 -new_net_names {net41} -new_cell_names {U407}
+insert_buffer [get_pins {U119/A}] BUF_X2 -new_net_names {net42} -new_cell_names {U408}
+size_cell {U373} {CLKBUF_X1}
+insert_buffer [get_pins {U131/A}] BUF_X2 -new_net_names {net43} -new_cell_names {U409}
+insert_buffer [get_pins {U396/A}] BUF_X1 -new_net_names {net44} -new_cell_names {U410}
+insert_buffer [get_pins {U376/A}] BUF_X2 -new_net_names {net45} -new_cell_names {U411}
+insert_buffer [get_pins {U151/Z}] BUF_X2 -new_net_names {net46} -new_cell_names {U412}
+size_cell {U390} {CLKBUF_X1}
+insert_buffer [get_pins {U321/A}] BUF_X2 -new_net_names {net47} -new_cell_names {U413}
+size_cell {U372} {CLKBUF_X1}
+insert_buffer [get_pins {U193/ZN}] BUF_X1 -new_net_names {net48} -new_cell_names {U414}
+insert_buffer [get_pins {pc_current_reg_13_/D}] BUF_X2 -new_net_names {net49} -new_cell_names {U415}
+insert_buffer [get_pins {U168/A}] BUF_X2 -new_net_names {net50} -new_cell_names {U416}
+insert_buffer [get_pins {pc_current_reg_12_/D}] BUF_X2 -new_net_names {net51} -new_cell_names {U417}
+size_cell {U400} {CLKBUF_X1}
+size_cell {U112} {CLKBUF_X1}
+insert_buffer [get_pins {U328/A}] BUF_X2 -new_net_names {net52} -new_cell_names {U418}
+size_cell {U378} {CLKBUF_X1}
+size_cell {U368} {CLKBUF_X1}
+size_cell {U139} {CLKBUF_X1}
+size_cell {U369} {CLKBUF_X1}
+size_cell {U375} {CLKBUF_X1}
+insert_buffer [get_pins {U382/A}] BUF_X2 -new_net_names {net53} -new_cell_names {U419}
+size_cell {U167} {CLKBUF_X1}
+size_cell {U331} {CLKBUF_X1}
+size_cell {U385} {CLKBUF_X1}
+insert_buffer [get_pins {U383/A}] BUF_X2 -new_net_names {net54} -new_cell_names {U420}
+insert_buffer [get_pins {U178/ZN}] BUF_X1 -new_net_names {net55} -new_cell_names {U421}
+size_cell {U84} {CLKBUF_X1}
+size_cell {U404} {CLKBUF_X1}
+size_cell {U74} {CLKBUF_X1}
+insert_buffer [get_pins {U405/A}] BUF_X1 -new_net_names {net56} -new_cell_names {U422}
+insert_buffer [get_pins {U113/A}] BUF_X2 -new_net_names {net57} -new_cell_names {U423}
+insert_buffer [get_pins {U392/A}] BUF_X2 -new_net_names {net58} -new_cell_names {U424}
+insert_buffer [get_pins {U320/A}] BUF_X2 -new_net_names {net59} -new_cell_names {U425}
+insert_buffer [get_pins {pc_current_reg_14_/D}] BUF_X2 -new_net_names {net60} -new_cell_names {U426}
+insert_buffer [get_pins {U389/A}] BUF_X2 -new_net_names {net61} -new_cell_names {U427}
+insert_buffer [get_pins {pc_current_reg_11_/D}] BUF_X2 -new_net_names {net62} -new_cell_names {U428}
+insert_buffer [get_pins {U169/A}] BUF_X1 -new_net_names {net63} -new_cell_names {U429}
+insert_buffer [get_pins {U395/A}] BUF_X2 -new_net_names {net64} -new_cell_names {U430}
+insert_buffer [get_pins {U398/A}] BUF_X2 -new_net_names {net65} -new_cell_names {U431}
+insert_buffer [get_pins {U336/A}] BUF_X2 -new_net_names {net66} -new_cell_names {U432}
+insert_buffer [get_pins {U201/A2}] BUF_X2 -new_net_names {net67} -new_cell_names {U433}
+insert_buffer [get_pins {pc_current_reg_8_/D}] BUF_X2 -new_net_names {net68} -new_cell_names {U434}
+size_cell {U379} {CLKBUF_X1}
+insert_buffer [get_pins {U412/A}] BUF_X2 -new_net_names {net69} -new_cell_names {U435}
+insert_buffer [get_pins {U386/A}] BUF_X2 -new_net_names {net70} -new_cell_names {U436}
+insert_buffer [get_pins {U402/A}] BUF_X2 -new_net_names {net71} -new_cell_names {U437}
+size_cell {U380} {CLKBUF_X1}
+insert_buffer [get_pins {U403/A}] BUF_X2 -new_net_names {net72} -new_cell_names {U438}
+insert_buffer [get_pins {U421/A}] BUF_X1 -new_net_names {net73} -new_cell_names {U439}
+insert_buffer [get_pins {pc_current_reg_3_/D}] BUF_X1 -new_net_names {net74} -new_cell_names {U440}
+insert_buffer [get_pins {U422/A}] BUF_X2 -new_net_names {net75} -new_cell_names {U441}
+insert_buffer [get_pins {U410/A}] BUF_X1 -new_net_names {net76} -new_cell_names {U442}
+insert_buffer [get_pins {U425/A}] BUF_X2 -new_net_names {net77} -new_cell_names {U443}
+insert_buffer [get_pins {U381/A}] BUF_X2 -new_net_names {net78} -new_cell_names {U444}
+insert_buffer [get_pins {U113/A}] BUF_X2 -new_net_names {net79} -new_cell_names {U445}
+insert_buffer [get_pins {U181/A2}] BUF_X2 -new_net_names {net80} -new_cell_names {U446}
+insert_buffer [get_pins {U84/Z}] BUF_X1 -new_net_names {net81} -new_cell_names {U447}
+insert_buffer [get_pins {U178/ZN}] BUF_X1 -new_net_names {net82} -new_cell_names {U448}
+insert_buffer [get_pins {U336/A}] BUF_X2 -new_net_names {net83} -new_cell_names {U449}
+insert_buffer [get_pins {U346/A}] BUF_X2 -new_net_names {net84} -new_cell_names {U450}
+size_cell {U413} {CLKBUF_X1}
+insert_buffer [get_pins {U74/Z}] BUF_X1 -new_net_names {net85} -new_cell_names {U451}
+size_cell {U388} {CLKBUF_X1}
+insert_buffer [get_pins {U141/A}] BUF_X1 -new_net_names {net86} -new_cell_names {U452}
+size_cell {U437} {CLKBUF_X1}
+insert_buffer [get_pins {pc_current_reg_7_/D}] BUF_X2 -new_net_names {net87} -new_cell_names {U453}
+size_cell {U116} {CLKBUF_X1}
+size_cell {U438} {CLKBUF_X1}
+size_cell {U434} {CLKBUF_X1}
+insert_buffer [get_pins {U380/A}] BUF_X2 -new_net_names {net88} -new_cell_names {U454}
+size_cell {U439} {CLKBUF_X1}
+size_cell {U414} {CLKBUF_X1}
+size_cell {U340} {CLKBUF_X1}
+size_cell {U347} {CLKBUF_X1}
+insert_buffer [get_pins {U133/A}] BUF_X1 -new_net_names {net89} -new_cell_names {U455}
+insert_buffer [get_pins {U95/A}] BUF_X1 -new_net_names {net90} -new_cell_names {U456}
+size_cell {U431} {CLKBUF_X1}
+size_cell {U393} {CLKBUF_X1}
+size_cell {U150} {CLKBUF_X1}
+size_cell {U424} {CLKBUF_X1}
+size_cell {U326} {CLKBUF_X1}
+size_cell {U120} {CLKBUF_X1}
+size_cell {U430} {CLKBUF_X1}
+insert_buffer [get_pins {U171/ZN}] BUF_X1 -new_net_names {net91} -new_cell_names {U457}
+insert_buffer [get_pins {U199/A2}] BUF_X2 -new_net_names {net92} -new_cell_names {U458}
+insert_buffer [get_pins {pc_current_reg_2_/D}] BUF_X1 -new_net_names {net93} -new_cell_names {U459}
+insert_buffer [get_pins {U202/A2}] BUF_X1 -new_net_names {net94} -new_cell_names {U460}
+size_cell {U410} {CLKBUF_X1}
+size_cell {U422} {CLKBUF_X1}
+size_cell {U394} {CLKBUF_X1}
+size_cell {U403} {CLKBUF_X1}
+size_cell {U446} {CLKBUF_X2}
+size_cell {U440} {CLKBUF_X1}
+size_cell {U432} {CLKBUF_X1}
+size_cell {U457} {CLKBUF_X1}
